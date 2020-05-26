5ae65495ad47dfe1ec44c25b24608835b8797c79
Matt Schroder <m3schroder@gmail.com>
2020-05-25 22:53:43 -0500

    Up to the messages screen

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,10 +1,13 @@
-import React from "react";
-import { FlatList, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { FlatList, StyleSheet, SafeAreaView, View } from "react-native";
+import Constants from "expo-constants";
 
 import ListItem from "../components/ListItem";
 import Screen from "../components/Screen";
+import ListItemSeperator from "../components/ListItemSeperator";
+import ListItemDeleteAction from "../components/ListItemDeleteAction";
 
-const messages = [
+const initialMessages = [
   {
     id: 1,
     title: "T1",
@@ -20,6 +23,14 @@ const messages = [
 ];
 
 function MessagesScreen(props) {
+  const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleDelete = (message) => {
+    // Delete the message from messages
+    setMessages(messages.filter((m) => m.id !== message.id));
+  };
+
   return (
     <Screen>
       <FlatList
@@ -30,11 +41,32 @@ function MessagesScreen(props) {
             title={item.title}
             subTitle={item.subTitle}
             image={item.image}
+            onPress={() => console.log("Message Sleected", item)}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+            )}
           />
         )}
+        ItemSeparatorComponent={ListItemSeperator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setMessages([
+            {
+              id: 2,
+              title: "T2",
+              description: "D2",
+              image: require("../assets/images/mosh.jpg"),
+            },
+          ]);
+        }}
       />
     </Screen>
   );
 }
 
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: Constants.statusBarHeight,
+  },
+});
 export default MessagesScreen;
